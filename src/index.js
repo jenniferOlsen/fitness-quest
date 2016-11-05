3bcc830124b411f305d5d2d7d1eb2e355261a3cd
Shetty, Raksha <Raksha.Shetty@nike.com>
2016-11-05 14:56:36 -0700

    Drop down colors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <App />,
